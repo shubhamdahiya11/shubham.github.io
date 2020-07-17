901c408532228c8f873bed7891166ba25c2e4cef
shubhamdahiya11 <shubhamdahiya11@gmail.com>
2020-07-17 06:14:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+$=(e=>document.querySelector(e)),document.addEventListener("DOMContentLoaded",()=>{var e=$("a-assets"),n=$("#splash");e.addEventListener("loaded",function(e){n.style.display="none"})});
